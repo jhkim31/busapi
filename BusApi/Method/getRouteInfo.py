410ea3781d96f70b08f7555f6b706588f33674d4
jhkim <biz.jh.kim@gmail.com>
2021-01-01 15:45:05 +0900

    v1.3 옵션값, 필수값 구분

--- BusApi/Method/getRouteInfo.py
+++ BusApi/Method/getRouteInfo.py
@@ -17,19 +17,61 @@ def getRouteInfo(routeId):
     
     routeId = jsons['response']['msgBody']['busRouteInfoItem']['routeId']
     routeName = jsons['response']['msgBody']['busRouteInfoItem']['routeName']
-    
-    operationInfo['downFirstTime'] = jsons['response']['msgBody']['busRouteInfoItem']['downFirstTime']
-    operationInfo['downLastTime'] = jsons['response']['msgBody']['busRouteInfoItem']['downLastTime']
+    try:
+        operationInfo['downFirstTime'] = jsons['response']['msgBody']['busRouteInfoItem']['downFirstTime']
+        #옵션값 
+    except:
+        
+        operationInfo['downFirstTime'] = "제공되지 않습니다."
+    try: 
+        operationInfo['downLastTime'] = jsons['response']['msgBody']['busRouteInfoItem']['downLastTime']
+        #옵션값 
+    except:
+        operationInfo['downLastTime'] = "제공되지 않습니다."
+        
     operationInfo['startStationId'] = jsons['response']['msgBody']['busRouteInfoItem']['startStationId']
+    #필수값 
     operationInfo['startStationName'] = jsons['response']['msgBody']['busRouteInfoItem']['startStationName']
-    operationInfo['startMobileNo'] = jsons['response']['msgBody']['busRouteInfoItem']['startMobileNo']
+    #필수값
+    try:
+        operationInfo['startMobileNo'] = jsons['response']['msgBody']['busRouteInfoItem']['startMobileNo']
+        # 옵션값 
+    except:
+        operationInfo['startMobileNo'] = "제공되지 않습니다." 
+        
+    
     operationInfo['endStationId'] = jsons['response']['msgBody']['busRouteInfoItem']['endStationId']
+    #필수값 
     operationInfo['endStationName'] = jsons['response']['msgBody']['busRouteInfoItem']['endStationName']
-    operationInfo['endMobileNo'] = jsons['response']['msgBody']['busRouteInfoItem']['endMobileNo']
-    operationInfo['upFirstTime'] = jsons['response']['msgBody']['busRouteInfoItem']['upFirstTime']
-    operationInfo['upLastTime'] = jsons['response']['msgBody']['busRouteInfoItem']['upLastTime']
-    operationInfo['peekAlloc'] = jsons['response']['msgBody']['busRouteInfoItem']['peekAlloc']
-    operationInfo['nPeekAlloc'] = jsons['response']['msgBody']['busRouteInfoItem']['nPeekAlloc']
+    #필수값
+    try:
+        operationInfo['endMobileNo'] = jsons['response']['msgBody']['busRouteInfoItem']['endMobileNo']
+        #옵션값 
+    except:
+        operationInfo['endMobileNo'] = "제공되지 않습니다."
+    
+    try:
+        operationInfo['upFirstTime'] = jsons['response']['msgBody']['busRouteInfoItem']['upFirstTime']
+        #옵션값 
+    except:
+        operationInfo['upFirstTime'] = "제공되지 않습니다."
+    
+    try:
+        operationInfo['upLastTime'] = jsons['response']['msgBody']['busRouteInfoItem']['upLastTime']
+        #옵션값 
+    except:
+        operationInfo['upLastTime'] = "제공되지 않습니다."
+        
+    try:
+        operationInfo['peekAlloc'] = jsons['response']['msgBody']['busRouteInfoItem']['peekAlloc']
+        #옵션값 
+    except:
+        operationInfo['peekAlloc'] = "제공되지 않습니다."
+        
+    try:
+        operationInfo['nPeekAlloc'] = jsons['response']['msgBody']['busRouteInfoItem']['nPeekAlloc']
+    except:
+        operationInfo['nPeekAlloc'] = "제공되지 않습니다. "
     
     returnData = {}
     returnData['routeId'] = routeId
